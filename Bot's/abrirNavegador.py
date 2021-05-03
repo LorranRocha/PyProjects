654e12e14de77124574b8687dffd94b91e7b314e
Lorran Rocha <63726045+LorranRocha@users.noreply.github.com>
2021-05-03 18:55:47 -0300

    Add files via upload

---
+++ Bot's/abrirNavegador.py
@@ -0,0 +1,11 @@
+import webbrowser as wb
+
+#   Created By: Lorran Rocha
+
+#   Abrir página no navegador // Open the page in a new browser window.
+
+wb.open('https://www.lorranrocha.com/')
+
+#   Abrir nova aba no navegador // Open the page in a new browser tab
+
+wb.open_new_tab('https://www.lorranrocha.com/')
